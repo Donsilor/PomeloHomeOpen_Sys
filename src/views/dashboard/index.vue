0cb5ba4c74d9a087bc950f5ef132ef87c0852431
huziyang <huziyang@evergrande.cn>
2017-12-14 09:24:45 +0800

    测试git

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,4 +1,5 @@
 <template>
+  <!--测试git-->
   <div class="dashboard-container">
     <div class='dashboard-text'>name:{{name}}</div>
     <div class='dashboard-text'>role:<span v-for='role in roles' :key='role'>{{role}}</span></div>

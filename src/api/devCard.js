b3707131e90c8161ce025b6db0098743aea55830
zhaochufeng <zhaochufeng@evergrande.com>
2021-03-26 10:17:03 +0800

    接口修改

--- src/api/devCard.js
+++ src/api/devCard.js
@@ -2,14 +2,14 @@ import fetch from '@/utils/fetch'
 
 export function rectCard(params){
   return fetch({
-    url: 'api/ext/devcard/rect',
+    url: '/api/ext/devcard/rect',
     method: 'get'
     // data: params
   })
 }
 export function rectPostCard(params){
   return fetch({
-    url: 'api/ext/devcard/rect',
+    url: '/api/ext/devcard/rect',
     method: 'post',
     data: params
   })

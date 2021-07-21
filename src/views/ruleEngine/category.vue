edc2a0a1e01103d99608d4fdc6345e8921d37b3c
donsilor <390807392@qq.com>
2021-07-21 11:39:23 +0800

    updata

--- src/views/ruleEngine/category.vue
+++ src/views/ruleEngine/category.vue
@@ -49,7 +49,7 @@
             label="修改时间 "/>
           <el-table-column
             label="操作"
-            width="340px"
+            width="320px"
           >
             <template slot-scope="scope">
               <div class="opreationBtn">
--- src/views/ruleEngine/scene.vue
+++ src/views/ruleEngine/scene.vue
@@ -57,6 +57,7 @@
           </el-table-column>
           <el-table-column
             label="操作"
+            width="320"
           >
             <template slot-scope="scope">
               <div class="opreationBtn">
